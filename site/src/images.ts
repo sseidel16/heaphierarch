46b9af57754a6820e411b8210b5144096b96b8a3
Stephen Seidel <stephen.a.seidel@gmail.com>
2025-12-31 00:02:27 -0700

    resize images to reasonable dimensions and quality for web

--- site/src/images.ts
+++ site/src/images.ts
@@ -19,15 +19,15 @@ export type ImageData = {
 }
 
 export const images: ImageData[] = [
-    { src: img001.src, width: img001.width, height: img001.height, focusX: 612, focusY: 237 },
-    { src: img002.src, width: img002.width, height: img002.height, focusX: 3475, focusY: 533 },
-    { src: img003.src, width: img003.width, height: img003.height, focusX: 1054, focusY: 1019 },
-    { src: img004.src, width: img004.width, height: img004.height, focusX: 554, focusY: 338 },
-    { src: img005.src, width: img005.width, height: img005.height, focusX: 819, focusY: 657 },
-    { src: img006.src, width: img006.width, height: img006.height, focusX: 3200, focusY: 1400 },
-    { src: img007.src, width: img007.width, height: img007.height, focusX: 1150, focusY: 1185 },
-    { src: img008.src, width: img008.width, height: img008.height, focusX: 700, focusY: 1300 },
-    { src: img009.src, width: img009.width, height: img009.height, focusX: 3550, focusY: 1550 },
-    { src: img010.src, width: img010.width, height: img010.height, focusX: 2569, focusY: 1843 },
-    { src: img011.src, width: img011.width, height: img011.height, focusX: 1229, focusY: 1866 },
+    { src: img001.src, width: img001.width, height: img001.height, focusX: 474, focusY: 184 },
+    { src: img002.src, width: img002.width, height: img002.height, focusX: 582, focusY: 92 },
+    { src: img003.src, width: img003.width, height: img003.height, focusX: 297, focusY: 267 },
+    { src: img004.src, width: img004.width, height: img004.height, focusX: 434, focusY: 262 },
+    { src: img005.src, width: img005.width, height: img005.height, focusX: 205, focusY: 163 },
+    { src: img006.src, width: img006.width, height: img006.height, focusX: 537, focusY: 234 },
+    { src: img007.src, width: img007.width, height: img007.height, focusX: 354, focusY: 358 },
+    { src: img008.src, width: img008.width, height: img008.height, focusX: 187, focusY: 335 },
+    { src: img009.src, width: img009.width, height: img009.height, focusX: 588, focusY: 251 },
+    { src: img010.src, width: img010.width, height: img010.height, focusX: 431, focusY: 311 },
+    { src: img011.src, width: img011.width, height: img011.height, focusX: 315, focusY: 466 },
 ];
